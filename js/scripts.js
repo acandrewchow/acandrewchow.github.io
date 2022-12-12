ab51f23837c441357e44c4ea643c90680c79ca53
Andrew <ac.andrewchow@gmail.com>
2022-12-12 15:26:11 -0500

    Added contet to blog, adjusted navigation on blog homepage

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,19 @@ $(document).ready(function(){
   });
 });
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
