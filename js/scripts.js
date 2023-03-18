0eb52dec7f5c6d0e4c9c18f2cda151104f016f6c
Andrew Chow <andrewchow@Andrews-MacBook-Pro.local>
2023-03-17 20:07:32 -0400

    adjusted bio and added scroll reveal library

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if(this.scrollY > 20) 
-      $(".navbar").addClass("sticky");
-    else
-      $(".navbar").removeClass("sticky");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) $(".navbar").addClass("sticky");
+    else $(".navbar").removeClass("sticky");
   });
 
-  $('.menu-toggler').click(function(){
+  $(".menu-toggler").click(function () {
     $(this).toggleClass("active");
     $(".navbar-menu").toggleClass("active");
   });
-});
+  
+  // JS type Library
+  var typed = new Typed(".auto-type", {
+    strings: ["Developer", "Creator", "Designer"],
+    typeSpeed: 125,
+    backSpeed: 125,
+    loop: true,
+  });
 
+  // Scroll Reveal Library
+  ScrollReveal().reveal("#about, #contact", {
+    delay: 100,
+    duration: 1000,
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
+    origin: "bottom",
+    distance: "50px",
+    threshhold: 0.5
+  });
+
+});
